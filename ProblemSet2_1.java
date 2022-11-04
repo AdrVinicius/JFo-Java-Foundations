71d2fbc6363f178420c203497c2f6e28550ca9f3
AdrVinicius <contatoadrian@outlook.com.br>
2022-11-04 18:19:34 -0300

    Create ProblemSet2_1.java

---
+++ ProblemSet2_1.java
@@ -0,0 +1,21 @@
+public class ProblemSet2_1 {
+    public static void main(String[] args) {
+        //Use print statements to create your own beautiful original ASCII art.
+        //Use comments to describe what your image is depicting.
+        
+     System.out.println( "   xxxxxxxxx  "); 
+     System.out.println( "  x          x "); 
+     System.out.println( "x   xx    xx   x"); 
+     System.out.println( "x              x"); 
+     System.out.println( "x    x    x    x"); 
+     System.out.println( "x     xxxx     x");  
+     System.out.println( "  x         x "); 
+     System.out.println( "    xxxxxxx   "); 
+
+        
+        
+        
+        
+        
+    } 
+}
