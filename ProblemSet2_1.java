fdff8fa43d34b9127e4a84e7523a3f3e1f1054d0
Adrian Vinicius <115122220+AdrVinicius@users.noreply.github.com>
2022-11-12 22:23:32 -0300

    Update ProblemSet2_1.java

--- ProblemSet2_1.java
+++ ProblemSet2_1.java
@@ -2,14 +2,14 @@ public class ProblemSet2_1 {
     public static void main(String[] args) {
      
         
-     System.out.println( "   xxxxxxxxx  "); 
-     System.out.println( "  x          x "); 
+     System.out.println( "   xxxxxxxxxx "); 
+     System.out.println( " x            x"); 
      System.out.println( "x   xx    xx   x"); 
      System.out.println( "x              x"); 
      System.out.println( "x    x    x    x"); 
      System.out.println( "x     xxxx     x");  
-     System.out.println( "  x         x "); 
-     System.out.println( "    xxxxxxx   "); 
+     System.out.println( " x            x"); 
+     System.out.println( "    xxxxxxxx  "); 
 
         
         
