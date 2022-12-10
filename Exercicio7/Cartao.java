225e7014624e52264e86e71a6324910d7b893a07
Adrian Vinicius <115122220+AdrVinicius@users.noreply.github.com>
2022-12-10 13:03:47 -0300

    Create Cartao.java

---
+++ Exercicio7/Cartao.java
@@ -0,0 +1,24 @@
+public class Cartao{
+
+    
+private int saldoAtual;
+private int saldoDeTiquetis;
+private int numeroCartao;
+
+public int getNumeroCartão(){
+    return numeroCartao;
+}
+public void setNumeroCartão(int novonúmeroCartao) {
+    this.numeroCartao = novonúmeroCartao;
+}
+public int getSaldoAtual() {
+    return saldoAtual;
+}
+public void setSaldoAtual(int saldoAtual) {
+    this.saldoAtual = saldoAtual;
+}
+public int getSaldoDeTiquetis() {
+    return saldoDeTiquetis;
+
+}
+}
