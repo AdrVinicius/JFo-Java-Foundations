5f83f3e5054bac952b29d1f39dfb6638c9b8df6d
Adrian Vinicius <115122220+AdrVinicius@users.noreply.github.com>
2022-12-10 13:04:42 -0300

    Create OCartao.java

---
+++ Exercicio7/OCartao.java
@@ -0,0 +1,23 @@
+public class OCartao {
+
+    private String nome;
+    private int quantidadeDeTiquetisTroca;
+    private int quantidadeDePremiosDisponivel;
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getQuantidadeDeTiquetisTroca() {
+        return quantidadeDeTiquetisTroca;
+    }
+    public void setQuantidadeDeTiquetisTroca(int quantidadeDeTiquetisTroca) {
+        this.quantidadeDeTiquetisTroca = quantidadeDeTiquetisTroca;
+    }
+    public int getQuantidadeDePremiosDisponivel() {
+        return quantidadeDePremiosDisponivel;
+    }
+    
+}
